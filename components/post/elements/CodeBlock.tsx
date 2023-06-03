56c136d9b755d36f89bb4c3e6f64d26fb3db1da6
spite400k <spite400k@gmail.com>
2023-06-04 07:27:01 +0900

    コンパイルエラー修正

--- components/post/elements/CodeBlock.tsx
+++ components/post/elements/CodeBlock.tsx
@@ -1,54 +1,54 @@
-import { CodeComponent } from "react-markdown/lib/ast-to-react";
-import { FC, ReactNode } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { nightOwl } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { CodeComponent } from 'react-markdown/lib/ast-to-react'
+import { FC, ReactNode } from 'react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { nightOwl } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 type wrapperprops = {
-  children: ReactNode;
-};
+  children: ReactNode
+}
 const CodeBlockWrapper: FC<wrapperprops> = ({ children }) => (
-  <div style={{ position: "relative" }}>{children}</div>
-);
+  <div style={{ position: 'relative' }}>{children}</div>
+)
 
 const CodeBlockTitle: FC<wrapperprops> = ({ children }) => (
   <div
     style={{
-      display: "inline-block",
-      position: "absolute",
-      top: "-1em",
+      display: 'inline-block',
+      position: 'absolute',
+      top: '-1em',
       left: 0,
-      backgroundColor: "#ccc",
-      padding: "0.2em",
-      color: "#000",
+      backgroundColor: '#ccc',
+      padding: '0.2em',
+      color: '#000'
     }}
   >
     {children}
   </div>
-);
+)
 
 const CodeBlock: CodeComponent = (props) => {
-  const { inline, className, children } = { ...props };
+  const { inline, className, children } = { ...props }
   if (inline) {
-    return <code className={className}>{children}</code>;
+    return <code className={className}>{children}</code>
   }
-  let match = /language-(\w+)(:.+)/.exec(className || "");
-  let lang = match && match[1] ? match[1] : "";
-  const name = match && match[2] ? match[2].slice(1) : "";
-  if (name === "") {
-    match = /language-(\w+)/.exec(className || "");
-    lang = match && match[1] ? match[1] : "";
+  let match = /language-(\w+)(:.+)/.exec(className || '')
+  let lang = match && match[1] ? match[1] : ''
+  const name = match && match[2] ? match[2].slice(1) : ''
+  if (name === '') {
+    match = /language-(\w+)/.exec(className || '')
+    lang = match && match[1] ? match[1] : ''
   }
-  if (lang === "gradle") {
-    lang = "kotlin";
+  if (lang === 'gradle') {
+    lang = 'kotlin'
   }
   return (
     <CodeBlockWrapper>
       {name && <CodeBlockTitle>{name}</CodeBlockTitle>}
       <SyntaxHighlighter style={nightOwl} language={lang}>
-        {String(children).replace(/\n$/, "")}
+        {String(children).replace(/\n$/, '')}
       </SyntaxHighlighter>
     </CodeBlockWrapper>
-  );
-};
+  )
+}
 
-export default CodeBlock;
+export default CodeBlock
