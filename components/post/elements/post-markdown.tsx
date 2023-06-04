1eba1c8848dc7abb99360c0f51de2bdaa1096244
spite400k <spite400k@gmail.com>
2023-06-05 04:05:37 +0900

    commit

--- components/post/elements/post-markdown.tsx
+++ components/post/elements/post-markdown.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import CodeBlock from './CodeBlock'
 import MdImage from './MdImage'
@@ -7,18 +6,6 @@ import { Post } from 'post/types/post'
 import matter from 'gray-matter'
 
 export const PostMarkdown = (props: { post: Post }) => {
-  // const [, setMarkdown] = useState('')
-
-  // useEffect(() => {
-  //   fetch(`/articles/test.md`)
-  //     .then((m) => {
-  //       return m.text()
-  //     })
-  //     .then((md) => {
-  //       setMarkdown(md)
-  //     })
-  // }, [])
-
   const { content } = matter(props.post.markdown ?? '')
   return (
     <ReactMarkdown
