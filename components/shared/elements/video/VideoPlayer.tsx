357005e4521393fb02488c19a240d78a4ffeaf87
spite400k <spite400k@gmail.com>
2023-09-13 11:05:09 +0900

    修正

--- components/shared/elements/video/VideoPlayer.tsx
+++ components/shared/elements/video/VideoPlayer.tsx
@@ -1,39 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import videojs from 'video.js';
+import React, { useState, useEffect } from 'react'
+import videojs from 'video.js'
 
-import 'video.js/dist/video-js.css';
+import 'video.js/dist/video-js.css'
 
-function VideoPlayer (props : videojs.PlayerOptions) {
-
-  const [player, setPlayer] = useState<videojs.Player>();
-  const videoNodeId = 'videojs-player';
+function VideoPlayer(props: videojs.PlayerOptions) {
+  const [player, setPlayer] = useState<videojs.Player>()
+  const videoNodeId = 'videojs-player'
 
   useEffect(() => {
-
-    setPlayer(videojs(videoNodeId, props).ready(function() {
-      // console.log('Player is Ready', this)
-    }));
-  }, [props]);
+    setPlayer(
+      videojs(videoNodeId, props).ready(function () {
+        // console.log('Player is Ready', this)
+      })
+    )
+  }, [props])
 
   useEffect(() => {
     return () => {
       if (player && !player.isDisposed()) {
-        player.dispose();
+        player.dispose()
       }
-    };
-  });
+    }
+  })
 
   return (
-    <div className="c-player" >
-      <div className="c-player__screen" data-vjs-player="true" data-setup='{}'>
-        /* eslint-disable-next-line jsx-a11y/media-has-caption */
-        <video
-          id={videoNodeId}
-          className="video-js"
-        />
+    <div className="c-player">
+      <div className="c-player__screen" data-vjs-player="true" data-setup="{}">
+        <video id={videoNodeId} className="video-js" />
       </div>
     </div>
-  );
+  )
 }
 
-export default React.memo(VideoPlayer);
+export default React.memo(VideoPlayer)
