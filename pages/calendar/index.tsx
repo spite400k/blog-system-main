c035e02ddadc8f34c22fa14a20fe1092e7282fea
spite400k <spite400k@gmail.com>
2023-09-13 09:47:48 +0900

    カレンダーのリスト表示

--- pages/calendar/index.tsx
+++ pages/calendar/index.tsx
@@ -7,11 +7,11 @@ import { FramerBox } from 'shared/elements/box/framer'
 import { CalendarType } from 'components/features/ calendar/types/calendar'
 import { CalendarHomeHeader } from 'components/features/ calendar/elements/list/calendar-home-header'
 import { CalendarListItem } from 'components/features/ calendar/elements/list/calendar-list-item'
-import FullCalendar from "@fullcalendar/react";
+import FullCalendar from '@fullcalendar/react'
 // FullCalendarで月表示を可能にするプラグイン。
-import dayGridPlugin from '@fullcalendar/daygrid';
+import dayGridPlugin from '@fullcalendar/daygrid'
 // 日本語対応のためのインポート
-import jaLocale from "@fullcalendar/core/locales/ja"; // 追加
+import jaLocale from '@fullcalendar/core/locales/ja' // 追加
 import { Tooltip } from 'shared/elements/tooltips'
 import { aa } from '@fullcalendar/core/internal-common'
 
@@ -29,25 +29,24 @@ const Home: NextPage = () => {
   })
 
   const calendarList = calendars.map((calendar) => {
-
-    const startDate = new Date(new Date(
-      calendar.start.seconds * 1000,
-    ).toLocaleDateString('ja-JP', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: 'numeric',
-      minute: 'numeric',
-    }));
-    const endDate = new Date(new Date(
-      calendar.end.seconds * 1000,
-    ).toLocaleDateString('ja-JP', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: 'numeric',
-      minute: 'numeric',
-    }));
+    const startDate = new Date(
+      new Date(calendar.start.seconds * 1000).toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: 'numeric',
+        minute: 'numeric'
+      })
+    )
+    const endDate = new Date(
+      new Date(calendar.end.seconds * 1000).toLocaleDateString('ja-JP', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: 'numeric',
+        minute: 'numeric'
+      })
+    )
 
     return {
       id: calendar.id,
@@ -56,15 +55,15 @@ const Home: NextPage = () => {
       end: endDate,
       description: calendar.description,
       backgroundColor: calendar.backgroundColor,
-      borderColor: calendar.borderColor,
-    };
-  });
+      borderColor: calendar.borderColor
+    }
+  })
 
   const EventComponent = (arg: aa) => (
     <Tooltip message={arg.event.title}>
       <div>{arg.event.title}</div>
     </Tooltip>
-	);
+  )
 
   return (
     <FramerBox>
@@ -72,11 +71,7 @@ const Home: NextPage = () => {
         <FlexBox way={'column'} width={'100%'} height={'100%'}>
           <CalendarHomeHeader />
           <FlexBox way={'row'} width={'100%'} height={'100%'} gap={'2em'}>
-
-            <Box
-              width={'50%'}
-              overflowY={'scroll'}
-            >
+            <Box width={'30%'} overflowY={'scroll'}>
               <FlexBox
                 width={'100%'}
                 way={'row'}
@@ -90,53 +85,53 @@ const Home: NextPage = () => {
               </FlexBox>
             </Box>
 
-            <FullCalendar
-              plugins={[dayGridPlugin]}
-              initialView="dayGridMonth"
-              events={calendarList}
-              locales={[jaLocale]}
-              locale='ja'
-              headerToolbar={{
-                start: "prev,today,next dayGridMonth,dayGridWeek,dayGridDay",
-                center: "title",
-                end: ""
+            <Box width={'70%'} overflowY={'scroll'}>
+              <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+                events={calendarList}
+                locales={[jaLocale]}
+                locale="ja"
+                headerToolbar={{
+                  start: 'prev,today,next dayGridMonth,dayGridWeek,dayGridDay',
+                  center: 'title',
+                  end: ''
                 }}
-              businessHours={true}
-              height={'auto'}
-              dayMaxEventRows={2}
-              expandRows={true}
-              aspectRatio={1.0}
-              // dateClick={handleDateClick}
-              // eventClick= {(e)=> {
-              //   //   alert('Event: ' + info.event.title + '¥r¥n'+'Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY + '¥r¥n'+'View: ' + info.view.type);
-              
-              //   //   // change the border color just for fun
-              //   //   info.el.style.borderColor = 'red';
-              //   //   return(
-              //   //     <>aaa</>
-              //   //   )
-              //   // }
-              //   console.log(e)
-              //   return( 
-              //     <ToolTip label="Tooltip text">
-              //       <h1>e</h1>
-              //       <h1>e.el.extendedProps.description</h1>
-              //     </ToolTip>)
-              //   }
-              // }
+                businessHours={true}
+                height={'auto'}
+                dayMaxEventRows={2}
+                expandRows={true}
+                aspectRatio={1.0}
+                // dateClick={handleDateClick}
+                eventClick={(e) => {
+                  //   alert('Event: ' + info.event.title + '¥r¥n'+'Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY + '¥r¥n'+'View: ' + info.view.type);
 
-              // eventDidMount= {(e)=> {
-              //   console.log(e)
-              //   return( 
-              //     <ToolTip label="Tooltip text">
-              //       <h1>e.event.title</h1>
-              //       <h1>e.el.extendedProps.description</h1>
-              //     </ToolTip>)
-              //   }
-              // }
-              
-              eventContent={(arg) => EventComponent(arg)}
-            />
+                  //   // change the border color just for fun
+                  //   info.el.style.borderColor = 'red';
+                  //   return(
+                  //     <>aaa</>
+                  //   )
+                  // }
+                  // console.log(e)
+                  return (
+                    <Tooltip message={'ツールチップ'}>
+                      <h1>e</h1>
+                      <h1>e.el.extendedProps.description</h1>
+                    </Tooltip>
+                  )
+                }}
+                // eventDidMount={(e) => {
+                //   console.log(e)
+                //   return (
+                //     <Tooltip label="Tooltip text">
+                //       <h1>e.event.title</h1>
+                //       <h1>e.el.extendedProps.description</h1>
+                //     </Tooltip>
+                //   )
+                // }}
+                // eventContent={(arg) => EventComponent(arg)}
+              />
+            </Box>
           </FlexBox>
         </FlexBox>
       </TayoriTemplate>
