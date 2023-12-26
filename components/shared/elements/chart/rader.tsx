08747a138c3bb3a0500bdc560bb407b3eb1ea6ff
spite400k <spite400k@gmail.com>
2023-12-26 16:01:28 +0900

    修正

--- components/shared/elements/chart/rader.tsx
+++ components/shared/elements/chart/rader.tsx
@@ -11,7 +11,7 @@ import {
 } from 'chart.js'
 import { Radar } from 'react-chartjs-2'
 import styles from './chart.module.css'
-import { MemberType } from '@/types/member'
+import { MemberType } from 'member/types/member'
 
 ChartJS.register(
   RadialLinearScale,
